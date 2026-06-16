700a1e7a2ea5e7026f1085b33fbb42326d1d5daf
aaplanzo <aaplanzo@gmail.com>
2026-06-16 12:08:49 -0400

    Update config.js

--- 05_api/config.js
+++ 05_api/config.js
@@ -2,4 +2,4 @@
 // Get your free sandbox key at: https://developer.va.gov/apply
 // Replace YOUR_API_KEY_HERE with your actual key
 
-const VA_API_KEY = "YOUR_API_KEY_HERE";
+const VA_API_KEY = "91hmgoQhiUbC0rM0ulfkx6zUetiqg4ka";
